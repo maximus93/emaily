a5d66214f4367687559e8cce5b420ac9e99aef72
himadri majumder <himadrimajumder8@gmail.com>
2019-08-27 00:23:39 +0530

    yoo

--- server/index.js
+++ server/index.js
@@ -5,8 +5,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
 
-//client_id 995906168481-6ludk26f1jt7v8v7kd5s96qv39789p9q.apps.googleusercontent.com
-//client secret PsoeeGJIF_DpyHhhnGVdEE5t
+
 
 
 passport.use(new GoogleStrategy());
