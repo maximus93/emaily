1a687c516ab6c691808feeaa4fc94f11b6d9763a
himadri majumder <himadrimajumder8@gmail.com>
2019-08-27 00:14:25 +0530

    yoo

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({Hi: 'sujit mod khawabe kobe?' });
 
-});
+//client_id 995906168481-6ludk26f1jt7v8v7kd5s96qv39789p9q.apps.googleusercontent.com
+//client secret PsoeeGJIF_DpyHhhnGVdEE5t
+
+
+passport.use(new GoogleStrategy());
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
